876c632b7cff7c6da8d9e9e1e92c789e3855e0d3
jainalisha14 <alisha@ryaz.io>
2025-02-19 11:39:18 +0530

    deleted unnecessary console outputs

--- src/API/ProcessApi.js
+++ src/API/ProcessApi.js
@@ -5,7 +5,6 @@ const API_URL = process.env.REACT_APP_API_URL;
 // Add a new process
 export const addProcess = async (processData) => {
   try {
-    console.log(processData);
     const response = await axios.post(`${API_URL}/add-process`, processData);
     return response.data;
   } catch (error) {
@@ -17,9 +16,6 @@ export const addProcess = async (processData) => {
 // Update an existing process by RSN and ProcessName
 export const updateProcess = async (RSN, ProcessName, updatedData) => {
   try {
-    console.log(RSN);
-    console.log(ProcessName);
-    console.log(updatedData);
     const response = await axios.put(`${API_URL}/update-process/${RSN}/${ProcessName}`, updatedData);
     return response.data;
   } catch (error) {
@@ -41,8 +37,6 @@ export const searchByRSN = async (RSN) => {
 
 // Delete a process by RSN and ProcessName
 export const deleteProcess = async (RSN, ProcessName) => {
-  console.log(RSN);
-  console.log(ProcessName);
   try {
     const response = await axios.delete(`${API_URL}/delete-process/${RSN}/${ProcessName}`);
     return response.data;
